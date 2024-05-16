c21d416d3379f1a769c9fdaad86fb9eabf8464b7
angembang <132391052+angembang@users.noreply.github.com>
2024-05-16 13:25:23 +0200

    Added error logging functionality to AddressManager class

--- managers/AddressManager.php
+++ managers/AddressManager.php
@@ -1,18 +1,20 @@
 <?php
 
 /**
- * Manages the retrieval and persistence of Address object in the platform
+ * Manages the retrieval and persistence of Address objects in the platform
  */
 class AddressManager extends AbstractManager
 {
   /**
-   * Creates a new Address and persits its in the database
+   * Creates a new Address and persists its in the database
    * 
    * @param Address $address The address object to be created.
    * 
    * @return Address The created address with the assigned identifier.
+   * 
+   * @throws PDOException If an error occurs during the database operation.
    */
-  public function createAddress(Address $address): ?Address
+  public function createAddress(Address $address): Address
   {
     try {
       // Prepare the SQL query to insert a new address into the database
@@ -33,7 +35,7 @@ class AddressManager extends AbstractManager
       // Execute the query with parameters
       $query->execute($parameters);
 
-      // Retrieves the last inserted idntifier
+      // Retrieve the last inserted identifier
       $addressId = $this->db->lastInsertId();
 
       // Set the identifier for the created address
@@ -42,8 +44,9 @@ class AddressManager extends AbstractManager
       // Return the created address object
       return $address;
     
-    } catch(Exception $e) {
-        throw new Exception("Failed to create address:");
+    } catch(PDOException $e) {
+      error_log("failed to create address" .$e->getMessage(),  $e->getCode());
+      throw new PDOException("Failed to create address");
     }
   }
 
@@ -53,7 +56,9 @@ class AddressManager extends AbstractManager
    * 
    * @param int $addressId the unique identifier of the address. 
    * 
-   * @return Address|null The retrieved address. Null if not found.
+   * @return Address|null The retrieved address, or null if not found.
+   * 
+   * @throws PDOException If an error occurs during the database operation.
    */
   public function findAddressById(int $addressId): ?Address
   {
@@ -83,10 +88,15 @@ class AddressManager extends AbstractManager
           $addressData["number"],
           $addressData["digicode_or_appt_name"]
         );
+        // Return the address object.
         return $address;
       }
-    } catch(Exception $e) {
-        throw new Exception("Failed to found address:");
+      // Address is not found, return null.
+      return null;
+    
+    } catch(PDOException $e) {
+      error_log("Failed to find address" .$e->getMessage(), $e->getCode());
+      throw new PDOException("Failed to find address");
     }
   }
 
@@ -94,9 +104,11 @@ class AddressManager extends AbstractManager
   /**
    * Retrieves addresses for a specific city
    * 
-   * @param string $city The city of the address. 
+   * @param string $city The city of addresses. 
    * 
-   * @return array|null The array of retrieved addresses. Null if not found
+   * @return array|null The array of retrieved addresses, or null if not found.
+   * 
+   * @throws PDOException If an error occurs during the database operation.
    */
   public function findAddressesByCity(string $city): ?array
   {
@@ -117,27 +129,165 @@ class AddressManager extends AbstractManager
 
       // Check if addresses data is not empty
       if($addressesData) {
-        $addresses = [];
-        // Loop through each address data
-        foreach($addressesData as $addressData) {
-          // Instantiate an address for each address data
-          $address = new Address(
-            $addressData["id"],
-            $addressData["city"],
-            $addressData["postal_code"],
-            $addressData["street"],
-            $addressData["number"],
-            $addressData["digicode_or_appt_name"]
-          );
-          // Add the instatiated Address to the addresses array.
-          $addresses[] = $address;
-        }
-        // Return the array of the address objects.
-        return $addresses;
-      }
-    } catch(Exception $e) {
-        throw new Exception("failed to found addresses:");
+        return $this->hydrateAddresses($addressesData);
+      } 
+      // No address is found, return null.
+      return null;
+    
+    } catch(PDOException $e) {
+      error_log("Failed to find addresses" .$e->getMessage(), $e->getCode());
+      throw new PDOException("Failed to find addresses");
     }
+  }
+
+
+  /**
+   * Retrieves all addresses
+   * 
+   * @return array Address|null The array of the retrieved address, or null if no address is found.
+   * 
+   * @throws PDOException If an error occurs during the database operation.
+   */
+  public function findAll(): ?array  
+  {
+    try {
+      // Prepare the SQL query to retrieve all addresses into the database.
+      $query = $this->db->prepare("SELECT * FROM addresses");
+
+      // Execute the query.
+      $query->execute();
+
+      // Fetch addresses data from the database
+      $addressesData = $query->fetchAll(PDO::FETCH_ASSOC);
+
+      // Check if addresses data is not empty
+      if($addressesData) {
+        return $this->hydrateAddresses($addressesData);
+      } 
+      // No address is found, return null.
+      return null;
+       
+    } catch(PDOException $e) {
+      error_log("Failed to find addresses" .$e->getMessage(), $e->getCode());
+      throw new PDOException("Failed to find addresses");
+    }
+  }
+
+
+  /**
+   * Updates an address in the database
+   * 
+   * @param Address $address The address to be updated.
+   * 
+   * @return Address|null The updated address, or null if not updated.
+   * 
+   * @throws PDOException If an error occurs during the database operation. 
+   */
+  public function updateAddress(Address $address): ?Address 
+  {
+    try {
+      // Prepare the SQL query to update the address.
+      $query = $this->db->prepare("UPDATE addresses SET 
+      city = :city,
+      postal_code = :postal_code,
+      street = :street,
+      number = :number,
+      digicode_or_appt_name = :digicode_or_appt_name 
+      WHERE id = :id ");
+
+      // Bind parameters with their values.
+      $parameters = [
+        ":id" => $address->getId(),
+        ":city" => $address->getCity(),
+        ":postal_code" => $address->getPostalCode(),
+        ":street" => $address->getStreet(),
+        ":number" => $address->getNumber(),
+        ":digicode_or_appt_name" => $address->getDigicodeOrApptName()
+      ];
+
+      // Execute the query with parameters.
+      $success = $query->execute($parameters);
+
+      // Check if success.
+      if($success) {
+        return $address;
+      } else {
+        return null;
+      }
+    
+    } catch(PDOException $e) {
+      error_log("Failed to update the address" .$e->getMessage(), $e->getCode());
+      throw new PDOException("Failed to update the address");
+    }
+  }
+
+
+  /**
+   * Deletes an address from the database
+   * 
+   * @param int $addressId The unique identifier of the address to delete.
+   * 
+   * @return bool True is successful, false if not.
+   * 
+   * @throws PDOException If an error occurs during the database operation. 
+   */
+  public function deleteAddressById(int $addressId): bool 
+  {
+    try {
+      // Prepare the SQL query to delete an address by its unique identifier
+      $query = $this->db->prepare("DELETE FROM addresses WHERE id = :id");
+
+      // Bind the parameter with its value.
+      $parameter = [
+        ":id" => $addressId
+      ];
+
+      // Execute the query with the parameter
+      $success = $query->execute($parameter);
+
+      // Check if success
+      if($success) {
+        return true;
+      } else {
+        return false;
+      }
+
+    } catch(PDOException $e) {
+      error_log("Failed to delete the address" .$e->getMessage(), $e->getCode());
+      throw new PDOException("Failed to delete the address");
+    }
+  }
+
+
+  /**
+   * Helper method to hydrate address objects from data array
+   * 
+   * @param array $addressesData The array of address data rtrieved from the database.
+   * 
+   * @return array An array of address objects hydrated from the provided data.
+   */
+  private function hydrateAddresses(array $addressesData): array 
+  {
+    // Initialize an empty array to store the hydrated Product objects.
+    $addresses = [];
+
+    // Loop through each product data in the array.
+    foreach($addressesData as $addressData) {
+      // Create a new Address object using the data from the current iteration.
+      $address = new Address(
+        $addressData["id"],          
+        $addressData["city"],        
+        $addressData["postal_code"],  
+        $addressData["street"],        
+        $addressData["number"],
+        $addressData["digicode_or_appt_name"]        
+      );
+    
+      // Add the newly created Product object to the array.
+      $addresses[] = $address;
+    }
+    // Return the array of hydrated Product objects.
+    return $addresses;
   }
 
 }
