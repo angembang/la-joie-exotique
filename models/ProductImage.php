5e98a85b146073e364f0661ea0fd00de9cdaa420
angembang <132391052+angembang@users.noreply.github.com>
2024-05-23 14:13:19 +0200

    feat: Remove id column from products_images table and update ProductImage model accordingly
    - Dropped the 'id' column from the 'products_images' table in the database.
    - Updated the ProductImage model to reflect these changes:
      - Removed the 'id' attribute from the class.
      - Updated the constructor to no longer accept 'id'.
      - Removed the 'getId' and 'setId' methods.
    - Refactored code to ensure compatibility with the updated ProductImage model.

--- models/ProductImage.php
+++ models/ProductImage.php
@@ -7,11 +7,6 @@
 class ProductImage 
 {
   /**
-   * @var int|null The unique identifier of the product image. Null for a new product image (not yet store in the database).
-   */
-  private ?int $id;
-
-  /**
    * @var int The product identifier of the product image.
    */
   private int $productId;
@@ -27,34 +22,13 @@ class ProductImage
    * @param int $productId The product identifier of the product image.
    * @param int $image The image identifier of the product image.
    */
-  public function __construct(?int $id, int $productId, int $imageId)
+  public function __construct(int $productId, int $imageId)
   {
-    $this->id = $id;
     $this->productId = $productId;
     $this->imageId = $imageId;
   }
 
-
-  /**
-   * Get the unique identifier of the product image
-   * 
-   * @return int|null The unique identifier of the product image. Null for a new product image
-   */
-  public function getId(): ?int
-  {
-    return $this->id;
-  }
-
-  /**
-   * Set the uniqque identifier of the product image
-   * @param int|null $id The unique identifier of the product image
-   */
-  public function setId(?int $id): void 
-  {
-    $this->id = $id;
-  }
-
-
+  
   /**
    * Get the product identifier of the product image
    * 
