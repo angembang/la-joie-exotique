0d51f157020c5595fd590290d771101b9019012a
angembang <132391052+angembang@users.noreply.github.com>
2024-05-28 13:40:57 +0200

    Instantiated neccessary controllers for displaying tamplates

--- services/Router.php
+++ services/Router.php
@@ -7,44 +7,6 @@
  class Router
 {
   /**
-   * @var PageController Instance of the PageController class.
-   */
-  private PageController $pageController;
-  /**
-   * @var ProductController Instance of the ProductController class.
-   */
-  private ProductController $productController;
-  /**
-   * @var AuthController Instance of the AuthController class.
-   */
-  private AuthController $authController;
-  /**
-   * @var ShowController Instance of the ShowController class.
-   */
-  private ShowController $showController;
-
-  /**
-   * Constructor
-   * 
-   * @param PageController $pageController The page controller instance.
-   * @param ProductController $productController The product controller instance.
-   * @param AuthController $authController The authentication controller instance.
-   * @param ShowController $showController The show controller instance.
-   */
-  public function __construct(
-    PageController $pageController, 
-    ProductController $productController, 
-    AuthController $authController, 
-    ShowController $showController)
-  {
-    $this->pageController = $pageController;
-    $this->productController = $productController;
-    $this->authController = $authController;
-    $this->showController = $showController;
-  }
-  
-  
-  /**
    * Handles the incoming request based on the provided $_GET parameters
    * 
    * @param array $get The associative array of $_GET parameters.
@@ -53,97 +15,103 @@
    */
   public function handleRequest(array $get): void 
   {
+    // Instanciate necessary controllers
+    $authController = new AuthController();
+    $pageController = new PageController();
+    $productController = new ProductController();
+    $showController = new ShowController();
+
     // Check if a route is provided
     if(isset($get["route"])) {
       switch($get["route"]) {
         case "register":
           // Route for displaying the register form
-          $this->authController->register();
+          $authController->register();
           break;
 
         case "checkRegister":
            // Route for checking the registration form submission
-          $this->authController->checkRegister();
+           $authController->checkRegister();
           break;
 
         case "login":
           // Route for displaying the login form
-          $this->authController->login();
+          $authController->login();
           break;
 
         case "checkLogin":
           // Route for checking the login form submission
-          $this->authController->checkLogin();
+          $authController->checkLogin();
           break;
 
         case "logout":
           // Route for logging out the user
-          $this->authController->logout();
+          $authController->logout();
           break;
 
         case "add-product":
           // Route for displaying the product addition form
-          $this->productController->addProduct();
+          $productController->addProduct();
           break;
 
         case "checkAddProduct":
           // Route for checking the product addition form submission
-          $this->productController->checkAddProduct();
+          $productController->checkAddProduct();
           break;
 
         case "search-product":
           // Route for searching products
-          $this->productController->searchProduct(); 
+          $productController->searchProduct(); 
           break;
 
         case "products":
           // Route for displaying all products
-          $this->showController->showProducts();
+          $showController->showProducts();
           break;
 
         case "category-products": 
           // Route for displaying products by category
-          $this->showController->showProductsByCategory();
+          $showController->showProductsByCategory();
           break;
 
         case "tag-products":
           // Route for displaying products by tag
-          $this->showController->showProductByTag();
+          $showController->showProductByTag();
           break;
 
         case "product-detail":
           // Route for displaying product details
-          $this->showController->showProductDetail(); 
+          $showController->showProductDetail(); 
           break;
 
         case "card":
           // Route for displaying the shopping cart
-          $this->showController->showCard();
+          $showController->showCard();
           break;
           
         case "order":
           // Route for placing an order
-          $this->productController->order();
+          $productController->order();
           break;
 
         case "confirm-order":
           // Route for confirming an order
-          $this->productController->confirmOrder();
+          $productController->confirmOrder();
           break;
 
         case "about":
           // Route for displaying the about page
-          $this->pageController->about();
+          $pageController->about();
           break;
 
         default:
           // Default route, display the home page
-          $this->pageController->home();
+          $pageController->home();
           break;
       }
     } else {
       // Route is not provided. Render the home page.
-      $this->pageController->home();
+      $pageController->home();
     }
   }
 }
